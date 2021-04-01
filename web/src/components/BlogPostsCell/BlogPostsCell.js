2d31cff5452cdfac5a21be10370e75d711b0c646
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-01 09:20:38 -0700

    Initial playing around

---
+++ web/src/components/BlogPostsCell/BlogPostsCell.js
@@ -0,0 +1,30 @@
+export const QUERY = gql`
+  query BlogPostsQuery {
+    posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => <div>Error: {error.message}</div>
+
+export const Success = ({ posts }) => {
+  return posts.map((post) => (
+    <article key={post.id}>
+      <header>
+        <h2>{post.title}</h2>
+      </header>
+
+      <div>{post.createdAt}</div>
+
+      <p>{post.body}</p>
+    </article>
+  ))
+}
---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+// import BlogLayout from 'src/layouts/BlogLayout'
+import BlogPostsCell from 'src/components/BlogPostsCell'
+
+const HomePage = () => {
+  return (
+    <>
+      <>
+        <p>Woot!</p>
+        <BlogPostsCell />
+      </>
+    </>
+  )
+}
+
+export default HomePage
