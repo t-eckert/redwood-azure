01efa64d030b6c459887d112ea9e135afddcc3f0
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-04-06 22:40:58 -0700

    Fix import into makeServices

--- api/hack-api/src/makeServices.ts
+++ api/hack-api/src/makeServices.ts
@@ -1,6 +1,4 @@
-// t-eckert: Just a stub for now, I guess?
-
-import { MakeServices } from "./types"
+import { MakeServices } from './types'
 
 export const makeServices: MakeServices = ({ services }) => {
   return services
